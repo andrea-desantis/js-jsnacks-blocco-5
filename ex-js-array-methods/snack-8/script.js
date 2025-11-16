baea63aae8c50afae908f7501f4d32f0278d8cce
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-16 17:09:57 +0100

    snack 8

--- ex-js-array-methods/snack-8/script.js
+++ ex-js-array-methods/snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classe = students.find((students) => {
+
+  return students.name ==='Marco Lanci';
+
+});
+
+console.log(classe.class);
