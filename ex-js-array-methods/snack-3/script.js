fa35afd22e86d26aa81e36717df079c0a9277940
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-16 16:56:19 +0100

    snack 3 &  4

--- ex-js-array-methods/snack-3/script.js
+++ ex-js-array-methods/snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+numbers.forEach (function(numbers){
+
+    console.log(numbers + 1);
+
+});
--- ex-js-array-methods/snack-4/script.js
+++ ex-js-array-methods/snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+posts.forEach(function(posts){
+
+    console.log(posts.author);
+});
